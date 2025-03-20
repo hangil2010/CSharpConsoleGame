05d9988d2de10e750806e4d53b5488a6df21fd0f
Hangil Chang <hangil2010@naver.com>
2025-03-20 16:36:58 +0900

    Relocated source code
    기존 코드를 새로 생성한 프로젝트 파일에 이동, 기존 코드 및 프로젝트 파일은 삭제

---
+++ ConsoleGame/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleGame
+{
+    internal class Program
+    {
+        // 2차원 좌표를 설정하는 구조체
+        struct Position
+        {
+            // x 좌표와 y 좌표를 설정, 좌상단을 기준으로 0, 0
+            public int x;
+            public int y;
